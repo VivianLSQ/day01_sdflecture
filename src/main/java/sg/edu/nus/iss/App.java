e1c0565fe9545ca21cd384acb0f3c2bc64f10b86
VivianLim <vivianlimsq@gmail.com>
2023-04-17 15:21:15 +0800

    initial commit

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,14 +1,84 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Console con = System.console();
+        String name = con.readLine("What is your name?");
+
+        if (name.length() > 0) {
+            System.out.printf("\nNice to meet you, %s", name);
+            System.out.println("\nNice to meet you, " + name);
+        } else {
+            System.out.println("You have not told me your name");
+        }
+
+        Integer age = 80;
+        System.out.printf("%s  %d years old.", name, age);
+        System.out.println("\n" + name + "is" + age + "years old");
+        System.out.print("Testing println, Name: " + name);
+
+        System.out.printf("%s is %d years old.", name, age);
+
+        // different ways to print the same content to console terminal
+        System.out.printf("\nNice to meet you, %s", name);
+
+        System.out.printf("%s is %d years old.", name, age);
+
+        String input = con.readLine("What is your hobby?");
+        input.trim(); // " swim " --> "swim"
+        if (input.equals("swim")) {
+            System.out.println(" The nearest swimming pool is Clementi");
+        } else if (input.equals("jog")) {
+            System.out.println("The nearest park is West Coast Park");
+        } else if (input.equals("cycle")) {
+            System.out.println("You can cycle along the PCN");
+        } else {
+            System.out.println("Seems like you are a boring person without hobby");
+        }
+
+        // Slide 18
+        // Switch Statement
+
+        switch (input) {
+            case "swim":
+                System.out.println(" The nearest swimming pool is in Clementi");
+                break;
+            case "jog":
+                System.out.println("The nearest park is West Coast Park");
+                break;
+            case "cycle":
+                System.out.println("You could cycle along PCN");
+                break;
+            default:
+                System.out.println("Seems like you are a boring person without hobby");
+                break;
+        }
+
+        // Slide 19
+        Integer myAge = 0;
+        String inputAge = con.readLine("What is your age?");
+        myAge = Integer.parseInt(inputAge);
+
+        if (myAge >= 0 && myAge <= 2) {
+            System.out.println("you are a baby");
+        } else if (myAge > 2 && myAge <= 4) {
+            System.out.println("you are a toddler");
+        } else if (myAge > 4 && myAge <= 12) {
+            System.out.println("You are a child");
+        } else if (myAge > 12 && myAge < 19) {
+            System.out.println("You are a teen");
+        } else {
+            System.out.println("you are an adult");
+        }
     }
 }
-git.init
